446c2c45dfcc67830a3e3e67966be7806cf56359
Artem Hordiyevskyy <139376262+slonic32@users.noreply.github.com>
2025-03-31 18:14:38 +0300

    Update SignInPage.jsx

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,27 +1,12 @@
-import { useState } from "react";
 import SignInForm from "../../components/SignInForm/SignInForm";
-import UpdateProfileForm from "../../components/UpdateProfileForm/UpdateProfileForm";
+import css from "./SignInPage.module.css";
 
 export default function SignInPage() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // Track login state
-
-  const handleLoginSuccess = () => {
-    console.log("Setting isLoggedIn to true...");
-    setIsLoggedIn(true); // Switch to profile update form after login
-  };
-
   return (
-    <main style={{ padding: "20px" }}>
-      {!isLoggedIn ? (
-        <div>
-          <SignInForm onLoginSuccess={handleLoginSuccess} />
-        </div>
-      ) : (
-        <div>
-          <h2>Update Profile</h2>
-          <UpdateProfileForm />
-        </div>
-      )}
+    <main>
+      <div>
+        <SignInForm />
+      </div>
     </main>
   );
 }
