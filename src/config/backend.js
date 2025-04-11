39bff74067d309a173b4476d50d494c8d60700aa
Artem Hordiyevskyy <139376262+slonic32@users.noreply.github.com>
2025-04-11 22:06:53 +0300

    github-pages

--- src/config/backend.js
+++ src/config/backend.js
@@ -1,2 +1,2 @@
-export const BACKEND_HOST = "http://localhost:3000";
-//export const BACKEND_HOST = "https://projectpilotbackend.onrender.com";
+//export const BACKEND_HOST = "http://localhost:3000";
+export const BACKEND_HOST = "http://141.144.239.176:3000";
